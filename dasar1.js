b5434433216fccec9dbf65ca64c4552576dcf32b
perompak_kode <iqbaltech29@gmail.com>
2023-08-31 10:35:36 +0700

    day  of Js

---
+++ dasar1.js
@@ -0,0 +1,53 @@
+// How to make variable
+var a = 'a'
+let b = 'b'
+const c = "c"
+
+// variable menampung nilai diwakilkan oleh nama variable
+
+let nama = "roruns"
+let umur = 69
+let isHuman = true
+let balance = 5000.055
+
+// Operasi Matematika
+
+let angka1 = 34
+let angka2 = 20
+
+console.log(angka1 + angka2, "Penambahan")
+console.log(angka1 - angka2 + "Pengurangan")
+console.log(`${angka1 / angka2} Pembagian`)
+console.log(angka1 * angka2 ,"Perkalian")
+console.log(angka1 % angka2, "Modulus / Sisa bagi")
+
+// Belajar mengisi variabel
+let penampungHasilOperasiAritmatika = angka1 + angka2
+console.log(penampungHasilOperasiAritmatika)
+penampungHasilOperasiAritmatika = angka1 - angka2
+console.log(penampungHasilOperasiAritmatika)
+penampungHasilOperasiAritmatika = angka1 / angka2
+console.log(penampungHasilOperasiAritmatika)
+penampungHasilOperasiAritmatika = angka1 * angka2
+console.log(penampungHasilOperasiAritmatika)
+penampungHasilOperasiAritmatika = angka1 % angka2
+console.log(penampungHasilOperasiAritmatika)
+
+let firstname = "Kafka"
+let lastname   = "Brutal"
+let fullname = firstname +" "+ lastname
+
+console.log(fullname);
+console.log(firstname + " " + lastname);
+
+
+let number = 20
+console.log(number + 10)
+number = 10
+console.log(number+10);
+number = false 
+console.log(number ? true : false)
+
+for (let i = 1 ; i < 21 ; i++){
+    console.log(`gas Habit ${i} of JS`);
+}
